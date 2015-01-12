97d580316a42ed9304432f6305476eaa7f5cbc76
PhiVanTran <tvphi1992@gmail.com>
2015-01-12 07:05:07 +0700

    Upload source code and image

---
+++ plot3.R
@@ -0,0 +1,20 @@
+dataFile <- "./DATA_R/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+## Converting dates
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+## Plot 3
+with(data, {
+  plot(Sub_metering_1~Datetime, type="l",
+       ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+## Saving to file
+dev.copy(png, file="./IMAGE_R/plot3.png", height=480, width=480)
+dev.off()
